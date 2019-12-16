e712af1b80fe80680077a9d759bc02c92a26afae
Adrian <szentkuti.adrian@gmail.com>
2019-12-16 10:27:06 +0100

    1.1

--- player.py
+++ player.py
@@ -1,9 +1,13 @@
 
 class Player:
-    VERSION = "1.0"
+    VERSION = "1.1"
 
     def betRequest(self, game_state):
-        return 0
+        me = game_state["in_action"]
+        if game_state["players"][me]["hole_cards"]["rank"] ==  game_state["players"][me]["hole_cards"]["rank"]:
+            return game_state["current_buy_in"] + game_state["minimum_raise"]
+        else:
+            return 0
 
     def showdown(self, game_state):
         pass
