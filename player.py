e426c5d9e2e1c5d3a19d9c7c3ddd00de23d97031
Adrian <szentkuti.adrian@gmail.com>
2019-12-16 10:34:47 +0100

    hehe

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
 
 class Player:
-    VERSION = "1.0"
+    VERSION = "1.1"
 
     def betRequest(self, game_state):
-        return 0
+        return 501
+
 
     def showdown(self, game_state):
         pass
