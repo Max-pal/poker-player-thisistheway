0341753b4da5850d04066fc388b20660443c4373
Adrian <szentkuti.adrian@gmail.com>
2019-12-16 10:56:39 +0100

    this is the way

--- player.py
+++ player.py
@@ -1,13 +1,18 @@
 
 class Player:
-    VERSION = "1.1"
+    VERSION = "1.2"
 
     def betRequest(self, game_state):
         me = int(game_state["in_action"])
-        if game_state["players"][me]["hole_cards"][0]["rank"] ==  game_state["players"][me]["hole_cards"][1]["rank"]:
-            return 200
+        card1 = game_state["players"][me]["hole_cards"][0]
+        card2 = game_state["players"][me]["hole_cards"][1]
+
+
+
+        if card1["rank"] == card2["rank"]:
+            return 100
         else:
-            return 0
+            return game_state["current_buy_in"]
 
     def showdown(self, game_state):
         pass
